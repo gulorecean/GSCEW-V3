c4e8a7a0664c926375cc3983ccc0752c5d7c8f86
gulorecean <79078916+gulorecean@users.noreply.github.com>
2023-04-09 15:18:16 +0200

    Add files via upload

---
+++ GSCEW.config.js
@@ -0,0 +1,16 @@
+import GSCEW from "./GSCEW_Lib/GSCEW.js"
+import Input_Scroll from "./GSCEW_Lib/Inputs/GSCEW_Input_Scroll.js"
+import Input_Mouse_Move from "./GSCEW_Lib/Inputs/GSCEW_Input_Mouse_Move.js"
+import WidgetFollowCursor from "./GSCEW_Lib/behaviours/WidgetFollowCursor.js"
+import WidgetMooveWithScroll from "./GSCEW_Lib/behaviours/WidgetMooveWithScroll.js"
+
+const Gestion = new GSCEW()
+Gestion.initInputs([
+    // your GSCEW inputs
+])
+Gestion.initBehaviours([
+    // your GSCEW Behaviours
+])
+Gestion.initObjects([
+    // yout DOM elements
+])
